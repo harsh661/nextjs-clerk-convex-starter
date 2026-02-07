bd6ba6de0dbdc447db4b057d81705010248d9164
harsh661 <plasmaharsh@gmail.com>
2026-02-07 08:30:39 +0530

    chore: auth page initial ui

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,35 @@
+"use client";
+
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <SignIn
+      appearance={{
+        layout: {
+          socialButtonsPlacement: "top",
+        },
+
+        variables: {
+          colorPrimary: "var(--primary)",
+          borderRadius: "8px",
+        },
+
+        elements: {
+          headerTitle: `!text-2xl`,
+          cardBox: `!shadow-none`,
+          card: `!bg-transparent !shadow-none !border-0 !p-4 !m-0`,
+
+          rootBox: "w-full",
+
+          button: `!py-2 !px-3 !text-sm`,
+
+          socialButtonsBlockButtonText: `!text-sm`,
+
+          input: `!py-2 !px-3 !text-sm !max-h-none !bg-[var(--card)]`,
+          footer: `!bg-none`,
+        },
+      }}
+    />
+  );
 }
